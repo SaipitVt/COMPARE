5a6c5ec0304aabbd111e1fa47002c05f7238e2e3
SaipitVt <online.saipit@gmail.com>
2026-02-04 02:52:15 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'price-compare-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json'
+];
+
+// Установка: кэшируем файлы
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Активация: удаляем старые кэши
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Запросы: берем из кэша, если интернета нет
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
